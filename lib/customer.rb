feebcc9ee5a291d9cae89928b97281d2db92dd3e
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-10-10 21:04:09 -0700

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,34 @@
 class Customer
-  
+    attr_reader :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(self, waiter, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    def waiters
+        Meal.all.select do |meal|
+            meal.customer == self
+        end.map do |meal|
+            meal.waiter
+        end
+    end
+
+    def self.all
+        @@all
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,42 @@
 class Waiter
+    attr_reader :name, :years_of_experience
+
+    @@all = []
+
+    def initialize(name, years_of_experience)
+        @name = name
+        @years_of_experience = years_of_experience
+        @@all << self
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(customer, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def best_tipper
+        maxTip = 0
+        biggestTipper = nil
+        
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end.each do |meal|
+            if meal.tip > maxTip
+                maxTip = meal.tip
+                biggestTipper = meal.customer
+            end
+        end
+
+        biggestTipper
+    end
+
+    def self.all
+        @@all
+    end
 
 end
